200723bc17994ac94ea20daff4d0093be38ae3ef
rajicdalibor <rajicdalibor@yahoo.com>
2020-11-20 11:07:23 +0100

    config edit

--- release.sh
+++ release.sh
@@ -7,19 +7,18 @@ RELEASE_PATTERN="^[0-9]+.([0-9]+|x)(.[0-9]+|.x)?$" # Matches patterns like 1.x, 
 
 if [[ "${CIRCLE_BRANCH}" =~ ${RELEASE_PATTERN} ]]; then
   VERSION=$(npm run version --prefix client --silent)
-#  echo ${VERSION}
-#
-#  echo "Configurate git"
-#  git config user.email "circleci@onstructive.ch"
-#  git config user.name "RELEASE"
-#
+
+  echo "Configurate git"
+  git config user.email "rajicdalibor@yahoo.com"
+  git config user.name "rajicdalibor"
+
   echo "Git commit release '${VERSION}'"
-#  git add client/package.json client/package-lock.json
-#  git commit -m "[skip ci] release '${VERSION}'"
-#  git tag ${VERSION}
-#
-#  git push --set-upstream origin $CIRCLE_BRANCH
-#  git push origin --tags
+  git add client/package.json client/package-lock.json
+  git commit -m "[skip ci] release '${VERSION}'"
+  git tag "${VERSION}"
+
+  git push --set-upstream origin "$CIRCLE_BRANCH"
+  git push origin --tags
   npm run version --prefix client
   echo "Releasing"
 else
